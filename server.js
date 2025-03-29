69084007ac458181a494f6e8bbe1947c47e09c36
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-29 14:05:49 +0600

    Setup mongoose to add mongodb database

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
+const port = process.env.port;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+
+    console.log("Successfully connected to mongodb database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-});
-
-app.listen(3000, () => {
-  console.log(`Server is listening on port 3000`);
 });
